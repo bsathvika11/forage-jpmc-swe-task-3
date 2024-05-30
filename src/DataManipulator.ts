ed2047f865d2532289d60f4658921b2c1581ed67
Sathvika <sathvikabandra11@gmail.com>
2024-05-30 18:26:23 +0530

    Committed changes in DataManipulator.ts,Graph.tsx,package.json

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,51 @@
-import { ServerRespond } from './DataStreamer';
+import { ServerRespond } from "./DataStreamer";
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc: Number;
+  price_def: Number;
+  ratio: Number;
+  timestamp: Date;
+  upper_bound: Number;
+  lower_bound: Number;
+  trigger_alert: Number | undefined;
 }
-
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(
+    serverRespond: ServerRespond[]
+    //historicalData: HistoricalData[]
+  ): Row {
+    //const averageRatio = calculateAverageRatio(historicalData);
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
+    const priceABC =
+      (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF =
+      (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const triggerAlert =
+      ratio > upperBound || ratio < lowerBound ? ratio : undefined;
+    const latestTimestamp =
+      serverRespond[0].timestamp > serverRespond[1].timestamp
+        ? serverRespond[0].timestamp
+        : serverRespond[1].timestamp;
+
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: latestTimestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: triggerAlert,
+    };
   }
 }
+/*function calculateAverageRatio(historicalData: HistoricalData[]): number {
+  let sumRatio = 0;
+  for (const data of historicalData) {
+    sumRatio += data.priceABC / data.priceDEF;
+  }
+  const averageRatio = sumRatio / historicalData.length;
+  return averageRatio;
+}*/
